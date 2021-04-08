e826bdde74a08f1f72648d8a70e30f4762ff9102
cqut-jia <8238469+cqut-jia@user.noreply.gitee.com>
2021-04-08 17:27:11 +0800

    3

--- 前端/foreman/src/views/工长详情/Details.vue
+++ 前端/foreman/src/views/工长详情/Details.vue
@@ -65,7 +65,19 @@
           
       </div>
   </div>
-   <div class="right"></div>
+   <div class="right">
+       <div class="name-title">
+           <strong>STAR</strong>
+           <h1 class="h1">明星工长</h1>
+           <h1 class="hh">张三三</h1>
+       </div>
+       <div class="content">
+          <p>各位业主大家好，我是好工长网认证的高级工长何本涛，大家可以叫我何工。
+   本人从事装修行业12年，自己有长期稳定专业的施工团队！精通室内设计施工各个环节，本人诚实守信，为客户节约成本。设计施工一条龙服务。先做人后做事，用良心做工程。
+   免费上门量房，出基础方案，做装修预算，解答装修常识。自觉做到重合同，守信用！
+   您放心把房交给我，回报您一个温馨、时尚、经济、适用的家，您的满意是我最大的愿望！</p>
+      </div>
+   </div>
   <div class="left-b">
       <div class="tit">
           <h2>预约我的业主</h2>
@@ -125,6 +137,57 @@ export default {
 </script>
 
 <style scoped>
+.right .content{
+    padding-top: 30px;
+    font-size: 14px;
+    line-height: 22px;
+}
+.hh{
+    height: 32px;
+    line-height: 32px;
+    padding: 0 10px;
+    color: #ffffff;
+    background: #01af63;
+    display: block;
+    font-size: 18px;
+    float: left;
+    margin-top: 40px;
+    font-weight: bold;
+}
+.h1{
+    height: 32px;
+    line-height: 32px;
+    padding: 0 10px;
+    color: #ffffff;
+    background: #000000;
+    display: block;
+    font-size: 18px;
+    font-weight: bold;
+    float: left;
+    margin-top: 40px;
+    margin-left: -12px;
+}
+.right .name-title{
+    padding-left: 20px;
+    height: 84px;
+    border-bottom: 1px solid #bbbbbb;
+    position: relative;
+}
+strong{
+    width: 100%;
+    color: #c9c9c9;
+    font-family: Arial;
+    font-size: 18px;
+    display: inline-block;
+    line-height: 26px;
+    margin-top: 8px;
+    font-weight: bold;
+    margin-top: -14px;
+    position: absolute;
+    left: 10px;
+    text-align: left;
+    top: 20px;
+}
 .ul{
     padding: 12px 0;
 }
@@ -152,10 +215,11 @@ export default {
     color: #505050;
 }
 .right{
+    width: 900px;
     float: right;
-    width: 935px;
-    border: 1px solid;;
-    height: 600px;
+    border: 1px solid #d6d6d6;
+    padding: 0 14px;
+    height: 538px;
 }
 .left-b{
     float: left;
